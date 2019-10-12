9eeacdf182717e6329f5c8ccc40648005e9355fd
jakubjakubiak <jebacyuu@gmail.com>
2019-10-13 01:13:31 +0200

    png=jpg

--- src/templates/designTemplate.js
+++ src/templates/designTemplate.js
@@ -10,9 +10,8 @@ const DesignTemplate = ({pageContext: {data} })=>(
   <div className="centerPost">
       <img src={data.image}/>
       <h1>{data.title}</h1>
-      <h2>{data.slug}</h2>
       <p>{data.description}</p>
-      <div>{data.div}</div>
+      {data.div}
       <p><a className="link" href={data.link}>{data.link}</a></p>
 
       <Link className="link" to="/"> Wróć do głownej  </Link>
